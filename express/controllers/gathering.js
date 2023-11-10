6c8032c9aff50d57cfed69f383ccab56fc731573
youngsupchoi <sup1214@naver.com>
2023-11-11 00:29:23 +0900

    feat: gathering

--- express/controllers/gathering.js
+++ express/controllers/gathering.js
@@ -49,6 +49,27 @@ exports.getChallengeGathering = async (req, res) => {
   }
 };
 
+exports.getCategory = async (req, res) => {
+  try {
+    const [rows] = await pool.query(`SELECT * FROM category`);
+    if (rows.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "카테고리가 아무것도 존재하지 않습니다!",
+      });
+    } else {
+      return res.status(200).json({ success: true, data: rows });
+    }
+  } catch (error) {
+    console.log("error: getCategory");
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "서버 에러",
+    });
+  }
+};
+
 exports.getHobbyGathering = async (req, res) => {
   try {
     const [rows] = await pool.query(
@@ -93,6 +114,32 @@ exports.createGathering = async (req, res) => {
     }
   } catch (error) {
     console.log("error: createGathering");
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "서버 에러",
+    });
+  }
+};
+
+exports.getGatheringNotice = async (req, res) => {
+  const { gathering_id } = req.params;
+  try {
+    const [rows] = await pool.query(
+      `SELECT * FROM gathering_notice where gathering_id = ?`,
+      [gathering_id]
+    );
+
+    if (rows.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "공지사항이 아무것도 존재하지 않습니다!",
+      });
+    } else {
+      return res.status(200).json({ success: true, data: rows });
+    }
+  } catch (error) {
+    console.log("error: getGatheringNotice");
     console.error(error);
     return res.status(500).json({
       success: false,
--- express/routes/gathering.js
+++ express/routes/gathering.js
@@ -11,4 +11,18 @@ router.get("/hobby", gathering.getHobbyGathering);
 
 router.get("/info/:id", gathering.getGatheringInfo);
 
+router.get("/category", gathering.getCategory);
+
+router.get("/notice/:gathering_id", gathering.getGatheringNotice);
+
+//router.get("/totalattend", gathering.getAtthend);
+
+//router.post("/post", gathering.postGatheringPost);
+
+//router.get("/best_user/:gathering_id", gathering.getBestUser);
+
+//router.get("/recentattendcount/:gathering_id", gathering.getRecentTotalAttend);
+
+//router.post("/postofactivity", gathering.getPostOfActivity);
+
 module.exports = router;
