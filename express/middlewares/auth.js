58299cfd1aa65b9e77ae02e9b8d8fc4d05605451
youngsupchoi <sup1214@naver.com>
2023-11-11 05:36:15 +0900

    feat: gathering

--- express/middlewares/auth.js
+++ express/middlewares/auth.js
@@ -2,31 +2,23 @@ const pool = require("../db");
 
 exports.isSignedIn = async (req, res, next) => {
   try {
-
-    if (req.body.hasOwnProperty("id")){
-
+    const authValue = req.headers["auth"];
+    if (req.headers["auth"]) {
       const sql = `SELECT id FROM user WHERE id = ?`;
-      const [rows] = await pool.query(sql, [req.body.id]);
-  
-      if (rows.length === 0){
-        
-        return res
-        .status(401)
-        .json({ success: false, message: "유효하지 않은 유저입니다!" });
+      const [rows] = await pool.query(sql, [req.headers["auth"]]);
 
-      }
-      else{
+      if (rows.length === 0) {
+        return res
+          .status(401)
+          .json({ success: false, message: "유효하지 않은 유저입니다!" });
+      } else {
         next();
       }
-    }
-    else{
-
+    } else {
       return res
         .status(401)
         .json({ success: false, message: "로그인이 필요합니다!" });
-
     }
-    
   } catch (error) {
     console.log("error: login");
     console.error(error);
