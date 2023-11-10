00226145b91a4398fff6e2cf9169b8675a7e831b
youngsupchoi <sup1214@naver.com>
2023-11-11 00:29:46 +0900

    fea: gatering

--- schema.sql
+++ schema.sql
@@ -7,17 +7,22 @@ COLLATE utf8mb4_general_ci;
 SET GLOBAL event_scheduler = ON;
 
 CREATE TABLE `category` (
-    `id` INT NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NULL,
     PRIMARY KEY (`id`)
 );
 
+INSERT INTO category values (1, "독서");
+INSERT INTO category values (2, "운동");
+INSERT INTO category values (3, "공부");
+INSERT INTO category values (4, "취미");
+
 CREATE TABLE `gathering` (
-    `id` INT NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `category_id` INT NOT NULL,
     `type` ENUM('challenge', 'hobby') NULL,
     `title` VARCHAR(255) NULL,
-    `created_at` TIMESTAMP NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     `image` BLOB NULL,
     `num_of_people` INT NULL,
     `introduction` TEXT NULL,
@@ -26,11 +31,11 @@ CREATE TABLE `gathering` (
 );
 
 CREATE TABLE `user` (
-    `id` INT NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(40) NOT NULL,
     `password` VARCHAR(40) NOT NULL,
     `name` VARCHAR(40) NOT NULL,
-    `created_at` TIMESTAMP NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     `image` BLOB,
     `company` VARCHAR(255),
     PRIMARY KEY (`id`)
@@ -38,10 +43,10 @@ CREATE TABLE `user` (
 
 
 CREATE TABLE `gathering_notice` (
-    `id` INT NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `gathering_id` INT NOT NULL,
     `content` TEXT NULL,
-    `created_at` TIMESTAMP NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`gathering_id`) REFERENCES `gathering` (`id`)
 );
@@ -55,17 +60,17 @@ CREATE TABLE `member` (
 );
 
 CREATE TABLE `gathering_activity` (
-    `id` INT NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `gathering_id` INT NOT NULL,
     `content` TEXT NULL,
-    `created_at` TIMESTAMP NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     `location` VARCHAR(255) NULL,
     `appointment_time` TIMESTAMP NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`gathering_id`) REFERENCES `gathering` (`id`)
 );
 CREATE TABLE `activity_attend` (
-    `id` INT NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `gathering_id` INT NOT NULL,
     `user_id` INT NOT NULL,
     PRIMARY KEY (`id`),
@@ -74,13 +79,15 @@ CREATE TABLE `activity_attend` (
 );
 
 CREATE TABLE `gathering_content` (
-    `id` INT NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `activity_id` INT NOT NULL,
     `user_id` INT NOT NULL,
-    `created_at` TIMESTAMP NULL,
+    `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     `content` TEXT NULL,
     `image` BLOB NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`activity_id`) REFERENCES `gathering_activity` (`id`),
     FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 );
+
+
