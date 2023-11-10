e615b9fe4de20a47cea140774a3526159ab10081
MS-LIMA <leems3256@gmail.com>
2023-11-11 05:51:10 +0900

    updated header

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 //import { PersonOutline, AssignmentInd } from "@mui/icons-material";
 
 const Header = () => {
+
+  const [isLogin, setIsLogin] = useState(localStorage.getItem('id') != '');
+
   return (
     <div className={styles.header}>
       <div className={styles.container}>
         <Link to={"/"} className={styles.link}>
           역삼모역
         </Link>
-        <div className={styles.nav}>
-          <p>소모임</p>
-          <p>챌린지</p>
-          <Link to={"/login"} className={styles.link}>
-            <Button variant="outlined">로그인</Button>
-          </Link>
-          <Link to={"/signup"} className={styles.link}>
-            <Button variant="outlined">회원가입</Button>
-            </Link>
-        </div>
+        {!isLogin ? 
+                <div className={styles.nav}>
+                <p>소모임</p>
+                <p>챌린지</p>
+                <Link to={"/login"} className={styles.link}>
+                  <Button variant="outlined">로그인</Button>
+                </Link>
+                <Link to={"/signup"} className={styles.link}>
+                  <Button variant="outlined">회원가입</Button>
+                  </Link>
+              </div>
+               : 
+               <div className={styles.nav}>
+               <p>소모임</p>
+               <p>챌린지</p>
+               <Button variant="outlined" onClick={() => {
+                  setIsLogin(false);
+                  localStorage.setItem('id', '');
+                 }}>로그아웃</Button>
+             </div>
+      }
+
       </div>
     </div>
   );
